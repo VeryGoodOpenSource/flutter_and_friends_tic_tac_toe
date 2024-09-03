082bb40bb649aa15bb57caa96952483a7eb85050
Salakar <mike.diarmid@gmail.com>
2024-09-03 09:55:02 +0100

    -

--- backend/lib/game.dart
+++ backend/lib/game.dart
@@ -2,26 +2,21 @@ import 'dart:async';
 
 import 'package:backend/redis.dart';
 
-/// A game server that manages a specific game and its state.
-/// TODO(workshopper): We will change the stream type.
-class Game extends StreamView<int> {
-  Game._(
-    int id, 
-    List<int?> fields, {
-    int player = 0,
-  }) : this.__(
-          id,
-          fields,
-          player,
-          StreamController<int>.broadcast(sync: true),
-        );
+enum GameState {
+  initial,
+  joining,
+  playing,
+}
 
-  Game.__(
+/// A game server that represents a game and its state.
+class Game {
+  /// A new game instance.
+  Game(
     this.id,
+    this.state,
     this.fields,
     this.player,
-    this._controller,
-  ) : super(_controller.stream);
+  );
 
   /// The player id of the current player.
   ///  0 = no player
@@ -29,6 +24,8 @@ class Game extends StreamView<int> {
   ///  2 = player 2
   final int player;
 
+  /// The current state of the game.
+  final GameState state;
 
   /// Null = no player moved.
   /// 1 = player 1 moved.
@@ -39,9 +36,6 @@ class Game extends StreamView<int> {
   /// This is currently just a incrementing number from Redis.
   /// It's not unguessable, but it's not a secret either.
   final int id;
-
-  // Internal StreamController used to broadcast game status updates.
-  final StreamController<int> _controller;
 
   /// Start new game and return the game id.
   /// Joining the game is done by calling [joinGame] separately.
@@ -61,10 +55,13 @@ class Game extends StreamView<int> {
     // TODO(workshopper): Implement determine player number.
     int player = 1;
 
+    // TODO(workshopper): Read from Redis state.
+    List<int?> fields = [];
+
     // TODO(workshopper): Parse the game state from Redis and return a Game
     // instance.
     // TODO(workshopper): Subscribe to Redis for updates.
-    return Game._(id, player: player);
+    return Game._(id, fields, player: player);
   }
 
   /// Validate and make move.
